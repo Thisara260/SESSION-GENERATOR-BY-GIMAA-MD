8726febd9bedc29d93ea586d5b3572c45f7c0006
Thisara260 <thisaragimhana011@gmail.com>
2025-08-12 15:42:49 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,5 +1,5 @@
 // get qr  code of any bot using this ....................
-//coded  by wasi
+//coded  by Gimaa
 
 
 const express = require("express");
@@ -17,14 +17,8 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *Gimaa-MD*
 ❒ _NOW DEPLOY IT_
-└────────────⭓
-┌───⭓
-❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
 └────────────⭓
 `
 
